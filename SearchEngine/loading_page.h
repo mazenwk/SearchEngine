47eaa01e2289e0d52dea52222b5551d4fa0b3f31
Mazen Kamel <mazenwkamel@aucegypt.edu>
2023-04-03 20:32:07 +0200

    Add Search Page

---
+++ SearchEngine/loading_page.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+#include <iostream>
+#include <string>
+
+#include "page.h"
+
+class loading_page : public page
+{
+public:
+	void display() {
+		display_logo();
+		display_searchbar();
+	}
+
+	void read_input() {
+		set_cursor_pos(40, 8);
+		std::string search_query;
+		std::cin >> search_query;
+		search(search_query);
+	}
+
+	void search(std::string& query) {
+
+	}
+
+private:
+	void display_logo() {
+		cout_centered("   __________  ____");
+		cout_centered("  / ____/ __ \\/ __ )");
+		cout_centered(" / / __/ / / / __  |");
+		cout_centered("/ /_/ / /_/ / /_/ /");
+		cout_centered("\\____/\\____/_____/ ");
+		cout_centered("google  on  budget  ");
+	}
+
+	void display_searchbar() {
+		cout_centered(" ____________________________________________");
+		cout_centered("/                                            \\");
+		cout_centered("|                                            |");
+		cout_centered("\\____________________________________________/");
+	}
+};
+
---
+++ SearchEngine/main.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+
+#include "loading_page.h"
+
+int main()
+{
+	auto lp = loading_page();
+	lp.display();
+	lp.read_input();
+}
