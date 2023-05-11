f052bf2976f5fdeead0b4f255cc9cfa926290d02
Mazen Kamel <107046923+mazenwk@users.noreply.github.com>
2023-05-11 16:31:59 +0300

    Implement loading bar

--- SearchEngine/UI/initialization_page.h
+++ SearchEngine/UI/initialization_page.h
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <chrono>
+#include <thread>
 
 #include "page.h";
 
@@ -13,7 +15,7 @@ public:
 		std::string warning = "Please ensure console width is at least 120 wide";
 		int line_width = 120 - warning.size() - 2;
 
-		display_vertical_spacing(15);
+		display_vertical_spacing(14);
 		std::cout << "|";
 		display_horizontal_line(line_width / 2);
 		std::cout << warning;
@@ -21,8 +23,10 @@ public:
 		std::cout << "|";
 		std::cout << '\n';
 
+		display_loading_bar();
+
 		while (true) {
-			set_cursor_pos(60, 16);
+			set_cursor_pos(60, 17);
 			char c = std::cin.get();  // Read a character from the input
 
 			if (c == '\n') {
@@ -53,6 +57,22 @@ private:
 		for (size_t i = 0; i < length; i++)
 		{
 			std::cout << "\n";
+		}
+	}
+
+	void display_loading_bar() {
+		cout_centered("Loading");
+		std::cout << "[";
+		for (int i = 0; i < 118; ++i) {
+			std::cout << " ";
+		}
+
+		std::cout << "]";
+
+		for (int i = 1; i <= 118; ++i) {
+			set_cursor_pos(i, 16);
+			std::cout << "=";
+			std::this_thread::sleep_for(std::chrono::milliseconds(25));
 		}
 	}
 };
